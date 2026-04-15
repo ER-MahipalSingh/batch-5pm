184a839a4f3459e88ce7d9602095c27633bf68ac
rwr1Mahipal <rwr1.mahipal.sb@gmail.com>
2026-04-15 17:45:15 +0530

    Add files via upload

---
+++ C/2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+    // int a = 10;
+    // float b = 20.25;
+    // float c = a + b;
+
+    // printf("%f",c);
+
+    // int a = 100;
+    // int b = 11;
+    // float c = (float)a / b;
+
+    // printf("%f", c);
+
+    // const int a = 10;
+    //     // a = 20;
+
+    //     printf("%d",a);
+
+    // int a = 10;
+    // a++;
+    // a++;
+    // ++a;
+    // a--;
+    // printf("%d",a);
+
+    // int a = 10;
+    // int b = 20;
+    // int c = a + b;
+
+    // printf("%d", c);
+    // printf("\n%d", a * b);
+    // printf("\n%d", a - b);
+    // printf("\n%d", a / b);
+
+    // printf("%d", a != b);
+
+    int num;
+    // printf("%dNumber: ",num);
+
+    printf("\nEnter a value: ");
+    scanf("%d", &num);
+
+    printf("\nNumber: %d", num);
+}
