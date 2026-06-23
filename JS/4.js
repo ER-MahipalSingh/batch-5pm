b8fd478d784faa7c3dc70596de75249a7fa2c673
MAHIPAL SINGH <rwr1.mahipal.sb@gmail.com>
2026-06-23 17:56:55 +0530

    Add files via upload

---
+++ JS/4.js
@@ -0,0 +1,20 @@
+const str = "Full Stack Developer ";
+const str1 = " and ";
+const str2 = "ML Engineer";
+const str3 = str.concat(str1, str2);
+
+// console.log(str.length);
+// console.log(str.charAt(10));
+// console.log(str.indexOf("S"));
+// console.log(str3);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.slice(0, 4));
+// console.log(str.trim());
+// console.log(str.includes("Q"));
+// console.log(str.substring(0,4)+"****");
+// console.log(str.replace("S", "H"));
+console.log(str.split());
+
+
+
