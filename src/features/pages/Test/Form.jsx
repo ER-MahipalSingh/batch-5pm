ed411a4c3203615f2b5755686a80f002c8c03a31
MAHIPAL SINGH <rwr1.mahipal.sb@gmail.com>
2026-09-24 17:38:48 +0530

    Add files via upload

--- src/features/pages/Test/Form.jsx
+++ src/features/pages/Test/Form.jsx
@@ -5,10 +5,23 @@ const Form = () => {
     name: "",
     email: "",
     password: "",
+    skills: [],
+    city: "",
   });
 
   const changeInputHandler = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    // setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const { name, value, type, checked } = e.target;
+
+    if (type === "checkbox") {
+      const addSkills = checked
+        ? [...formData.skills, value]
+        : formData.skills.filter((skill) => skill !== value);
+      setFormData({ ...formData, skills: addSkills });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
   };
 
   const formSubmitHandler = (e) => {
@@ -46,6 +59,52 @@ const Form = () => {
             placeholder="Enter your password"
             onChange={changeInputHandler}
           />
+        </div>
+
+        <div>
+          <label htmlFor="skills">Skills</label>
+
+          <div>
+            <label htmlFor="react">React</label>
+            <input
+              type="checkbox"
+              name="skills"
+              value="react"
+              onChange={changeInputHandler}
+              checked={formData.skills.includes("react")}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="node">Node</label>
+            <input
+              type="checkbox"
+              name="skills"
+              value="node"
+              onChange={changeInputHandler}
+              checked={formData.skills.includes("node")}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="javaScript">JavaScript</label>
+            <input
+              type="checkbox"
+              name="skills"
+              value="javaScript"
+              onChange={changeInputHandler}
+              checked={formData.skills.includes("javaScript")}
+            />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="City">City</label>
+          <select name="city" id="city" onChange={changeInputHandler}>
+            <option value="rajkot">Rajkot</option>
+            <option value="surat">Surat</option>
+            <option value="baroda">Baroda</option>
+          </select>
         </div>
 
         <button type="submit">Submit</button>
